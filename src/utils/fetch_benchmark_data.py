d30f446ec38bfb1fb48f9a7faa12dee071eec710
nwq388 <nwq388@hendrixgate01fl.unicph.domain>
2026-04-06 10:32:44 +0200

    Fetching 201-301 models

--- src/utils/fetch_benchmark_data.py
+++ src/utils/fetch_benchmark_data.py
@@ -59,7 +59,7 @@ dataset = load_dataset("open-llm-leaderboard/contents", split="train")
 dataset_df = dataset.to_pandas()
 ranked_df = dataset_df.sort_values(by='Average ⬆️', ascending=False, ignore_index=True)
 ranked_df_full_name = ranked_df["fullname"].tolist()
-top100_ranked_df_full_names = ranked_df_full_name[101:201]
+top100_ranked_df_full_names = ranked_df_full_name[201:301]
 
 
 pattern = r'(\d+(?:\.\d+)?)[Bb]'
@@ -72,7 +72,8 @@ for model_name in top100_ranked_df_full_names:
 
 print ("there are ", len(filtered_model_names), "models under 20B in the top 100")
 print ('" "'.join(filtered_model_names))
-print ("~~~~~~~~~~~~~~~~~~~~")   
+print ("~~~~~~~~~~~~~~~~~~~~")
+print("25 from list: ", filtered_model_names[:25])   
 
 
 
